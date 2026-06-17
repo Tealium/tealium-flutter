c7c2d46e2ed8d23df0a5b54c0a6eb8893fc4a91f
Sebastian Krajna <sebastian.krajna@tealium.com>
2026-06-17 14:27:36 +0200

    [MT-2002] Use .v13 platform version syntax in Package.swift

--- tealium/ios/tealium/Package.swift
+++ tealium/ios/tealium/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "tealium",
     platforms: [
-        .iOS("13.0")
+        .iOS(.v13)
     ],
     products: [
         .library(name: "tealium", targets: ["tealium"])
